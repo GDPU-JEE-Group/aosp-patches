383fa1747902544a8f365f5362deeeafcbd160ed
chaixiang <973731820@qq.com>
2024-08-19 08:33:44 +0000

    更新创建feature脚本

--- sh/new-feature.sh
+++ sh/new-feature.sh
@@ -1,15 +1,12 @@
-
-
-sourse a_patches/sh/util.sh
+source a_patches/sh/util.sh
 feature_branch=$1
 template_branch=$2
 
 check_args() {
     # 检查是否有任意一个参数为空
-    if [ -z "$1" ] || [ -z "$2" ] ; then
+    if [ -z "$feature_branch" ] || [ -z "$feature_branch" ] ; then
         # 打印示例信息
-        # echo "Usage: $0 <arg1> <arg2> <arg3>"
-        # echo "Example: $0 value1 value2 value3"
+        echo " - : $0 $feature_branch $feature_branch"
         echo "Usage: $0 feature_branch template_branch"
         echo "Example: $0 ft-ins dev-20240815"
         # 退出脚本
@@ -32,15 +29,18 @@ confirm_clear_operation(){
     fi
 }
 
-
 switch_branch(){
     # 检查远程分支是否存在
     if git ls-remote --exit-code --heads origin $template_branch; then
         # git 以某个分支为模板，创建一个新分支并切换到新分支
-        run_cmd git checkout -b $feature_branch $template_branch
+        run_cmd git fetch origin $template_branch:refs/remotes/origin/$template_branch
+        run_cmd git checkout -b $feature_branch origin/$template_branch
     else
         echo -e "\033[0;31mError: $template_branch no error！ \033[0m"
     fi
 }
 
-
+#####################################################################
+check_args
+confirm_clear_operation
+switch_branch
